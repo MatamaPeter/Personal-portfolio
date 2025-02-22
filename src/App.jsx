e1a5bc0bbfe34fd855d97b38fdba7c540646453c
MatamaPeter <peter24matama@gmail.com>
2025-02-22 15:13:48 +0300

    Added motion hook and disabled scrollbars for smooth transition.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Header from "./Components/Header";
 import './App.css';
+import { motion } from "motion/react";
+
 
 
 function App() {
   return (
     <>
-      <Header/ >
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{duration:0.5}}
+      
+      >
+      <Header />
+      </motion.div>
     </>
   )
 }
