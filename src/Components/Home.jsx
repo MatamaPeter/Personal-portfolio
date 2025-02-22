8ae5c5908658b7af7a1ee24fb5fc040044b09934
MatamaPeter <peter24matama@gmail.com>
2025-02-23 00:09:31 +0300

    Adde latest blog section to complete the homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,15 @@
-import Portfolio from './Home_components/Portfolio.jsx';
 import HeroSection from './Home_components/Hero_Section.jsx';
+import Portfolio from './Home_components/Portfolio.jsx';
+import LatestBlogs from './Home_components/LatestBlogs.jsx';
+
 import './home.css'
 function Home() {
   return (
     <div className='home-container'>
+
+
       <HeroSection />
+
       <div className="portfolio-container">
         <h1>Portfolio</h1>
         <div className="portfolio-elements">
@@ -13,6 +18,22 @@ function Home() {
           <Portfolio />
         </div>
       </div>
+
+
+      <div className="blog-container">
+        <h1>
+          <a href="">Latest Blogs</a>
+          <i className="material-icons" >arrow_outward</i>
+        </h1>
+        <span>
+          I occasionally write about programming, productivity, and more. <br />
+          Check me out and subscribe to stay up to date.
+        </span>   
+        <div className="latest-blog-elements">
+          <LatestBlogs />
+        </div>
+      </div>
+
     </div>
   )
 }
---
+++ src/Components/Home_components/LatestBlogs.jsx
@@ -0,0 +1,21 @@
+function LatestBlogs() {
+  return (
+    <div>
+        <div className="blog-dtls">
+              <div className="blog-section-desc"> 
+                  <div className="post-date">
+                      Oct 27, 2023
+                  </div>
+                  <div className="post-desc">
+                      My Desk Setup for 2023. Minimalist but functional.
+                  </div>
+              </div>  
+              <div className="blog-img">
+                  <img src="computer.webp" alt="" />
+              </div>
+        </div>
+    </div>
+  )
+}
+
+export default LatestBlogs
