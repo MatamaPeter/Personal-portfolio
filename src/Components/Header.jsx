a734e3c50c2b00995ba4c1a673b81b9d2bfd39dc
MatamaPeter <peter24matama@gmail.com>
2025-02-22 15:04:56 +0300

    Completed the header section adding light and dark mode functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,31 +1,50 @@
-import { useState } from "react";
-import "./Header.css";
-
+import { useState } from 'react';
+import './Header.css';
 function Header() {
-    const [navOpen, setNavOpen] = useState(false);
 
-    return (
-        <div className="header-container">
-            <img src="my_portfolio_logo_light.svg" alt="My-logo" className="logo" />
-            
-            <i className="material-icons menu-icon" onClick={() => setNavOpen(!navOpen)}>
-                {navOpen ? "close" : "menu"}
-            </i>
+    // Global Variables
+    const tabs = ["Home", "About", "Plans", "Blogs"]
+    
+    // States
+    const [isActive, setIsActive] = useState("Home");
+    const [darkMode, setDarkMode] = useState(true);
 
-            <div className={`nav-bar ${navOpen ? "open" : ""}`}>
-                <ul>
-                    <li className="active"><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#plans">Future Plans</a></li>
-                    <li><a href="#blogs">Blogs</a></li>
-                </ul>
-            </div>
 
-            <i className="material-icons dark-mode-icon" onClick={() => document.body.classList.toggle("dark-theme")}>
-                dark_mode
-            </i>
-        </div>
-    );
+    //Callback functions    
+   const tabsElement = tabs.map((tab, index) => {
+            return(
+                <li
+                    key={index}
+                    className={isActive===tab ? "active": ""}
+                    onClick={() => setIsActive(tab)}
+                >
+                <a href="#">{tab}</a></li>
+            )
+    })
+
+    function toggleDarkMode() {
+        setDarkMode(!darkMode);
+        document.body.classList.toggle("light-theme");
+    }
+    
+    
+    
+    
+
+  return (
+      <>
+          <div className="header-container">
+              <img src={darkMode ? "my_portfolio_logo_light.svg" : "my_portfolio_logo_dark.svg"} alt="My-logo" className="logo" />
+              <div className='nav-bar'>
+                  <ul>
+                      {tabsElement}
+                  </ul>
+              </div>
+              <i className="material-icons" onClick={toggleDarkMode}>{darkMode?"dark_mode":"light_mode"}</i>
+
+          </div>
+      </>
+  )
 }
 
-export default Header;
+export default Header
