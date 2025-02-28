6c1cceb02ded983c3d51bcc86480e3509e0c64f8
MatamaPeter <peter24matama@gmail.com>
2025-02-28 17:58:42 +0300

    switched the available for work button

--- src/Components/Home_components/Hero_Section.jsx
+++ src/Components/Home_components/Hero_Section.jsx
@@ -1,11 +1,9 @@
 export default function HeroSection() {
     return (
         <div className="hero-container">
-            <div className="availability-status">
-                <i className="material-icons">fiber_manual_record</i>
-                Available for work
-            </div>
+            
             <h1>Hey, Matama here!</h1>
+            
             <p>
                 I am a passionate software engineer committed to pushing the limits of technology. I continuously seek new challenges to expand my skills and contribute to impactful projects.
             </p>
@@ -22,6 +20,10 @@ export default function HeroSection() {
                     <a href="resume.pdf">Resume</a>
                     <i className="material-icons" >arrow_downward</i>
                 </div>
+            </div>
+            <div className="availability-status">
+                <i className="material-icons">fiber_manual_record</i>
+                Available for work
             </div>
         </div>
     )
