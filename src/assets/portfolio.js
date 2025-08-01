45fb960224a9d421cdecca9c76b01a61849e085e
MatamaPeter <peter24matama@gmail.com>
2025-08-01 18:49:12 +0300

    Refactor project entries in portfolio.js for improved readability and consistency

--- src/assets/portfolio.js
+++ src/assets/portfolio.js
@@ -1,61 +1,66 @@
-
 const projects = [
-    {
-        id: "1",
-        title: "Chef's Mate",
-        description: "An AI-powered recipe generator to suggest recipes based on your own ingredients",
-        image: "assets/chef-mate.png", 
-        link: "https://chef-s-mate.vercel.app/",
-    },
-    {
-        id: "2",
-        title: "Portfolio",
-        description: "My personal portfolio built using React. Carrying most of my details.",
-        image: "assets/portfolio.png",
-        link: "https://personal-portfolio-ruddy-mu-10.vercel.app/",
-    },
-    {
-        id: "3",
-        title: "Tenzies App",
-        description: "A boredom curing game, similar to rolling a dice created with react.",
-        image: "assets/tenzies.png",
-        link: "https://tenzies-red.vercel.app/",
-    },
-    {
-        id: "4",
-        title: "Assembly game",
-        description: "Mind sharpening game which involves guessing letters to create a complete word.",
-        image: "assets/assembly-game.png",
-        link: "https://assembly-game-kappa.vercel.app/",
-    },
-    {
-        id: "5",
-        title: "Miritec Gym and fitness",
-        description: "An online store platfrom for gym and fitness equipment based in Kenya.",
-        image: "assets/miritec.png",
-        link: "https://miritecgymandfitnesskenya.co.ke/",
-    },
-    {
-        id: "6",
-        title: "Travel journal",
-        description: "A react app created to allow travel journal keeping.",
-        image: "assets/travel-journal.png",
-        link: "https://travel-journal-virid-seven.vercel.app/",
-    },
-    {
-        id: "7",
-        title: "Epicks",
-        description: "A multi-products online store created with dynamic features",
-        image: "assets/Epicks.jpeg",
-        link: "https://github.com/MatamaPeter/1-project",
-    },
-    {
-        id: "8",
-        title: "Farmicom",
-        description: "A farm-products-based ecommerce website together with its corresponding dashboards.",
-        image: "assets/farmicom.jpeg",
-        link: "https://github.com/MatamaPeter/FARMICOM",
-    }
+  {
+    id: "1",
+    title: "Chef's Mate",
+    description:
+      "An AI-powered recipe generator to suggest recipes based on your own ingredients",
+    image: "assets/chef-mate.png",
+    link: "https://chef-s-mate.vercel.app/",
+  },
+  {
+    id: "2",
+    title: "Portfolio",
+    description:
+      "My personal portfolio built using React. Carrying most of my details.",
+    image: "assets/portfolio.png",
+    link: "https://personal-portfolio-ruddy-mu-10.vercel.app/",
+  },
+  {
+    id: "3",
+    title: "Tenzies App",
+    description:
+      "A boredom curing game, similar to rolling a dice created with react.",
+    image: "assets/tenzies.png",
+    link: "https://tenzies-red.vercel.app/",
+  },
+  {
+    id: "4",
+    title: "Assembly game",
+    description:
+      "Mind sharpening game which involves guessing letters to create a complete word.",
+    image: "assets/assembly-game.png",
+    link: "https://assembly-game-one.vercel.app/",
+  },
+  {
+    id: "5",
+    title: "Miritec Gym and fitness",
+    description:
+      "An online store platfrom for gym and fitness equipment based in Kenya.",
+    image: "assets/miritec.png",
+    link: "https://miritecgymandfitnesskenya.co.ke/",
+  },
+  {
+    id: "6",
+    title: "Travel journal",
+    description: "A react app created to allow travel journal keeping.",
+    image: "assets/travel-journal.png",
+    link: "https://travel-journal-virid-seven.vercel.app/",
+  },
+  {
+    id: "7",
+    title: "Epicks",
+    description: "A multi-products online store created with dynamic features",
+    image: "assets/Epicks.jpeg",
+    link: "https://github.com/MatamaPeter/1-project",
+  },
+  {
+    id: "8",
+    title: "Farmicom",
+    description:
+      "A farm-products-based ecommerce website together with its corresponding dashboards.",
+    image: "assets/farmicom.jpeg",
+    link: "https://github.com/MatamaPeter/FARMICOM",
+  },
 ];
 
 export default projects;
